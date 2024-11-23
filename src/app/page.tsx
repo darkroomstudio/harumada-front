48be149f12787709295d1b3901d86e48f9196a3f
Jiwon Choi <devjiwonchoi@gmail.com>
2024-11-23 17:09:06 +0900

    progress card (#10)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import ProgressCard from '@/components/custom/progress-card'
 async function getProgressCards() {
   const cards = [
     {
+      id: '0',
       dueDate: '2024-06-30',
       progress: 0.2,
       title: '스쿠버 다이빙 배우기',
@@ -10,6 +11,7 @@ async function getProgressCards() {
       totalDays: 30,
     },
     {
+      id: '1',
       dueDate: '2024-08-15',
       progress: 0.4,
       title: '오픈워터 자격증 취득하기',
@@ -17,6 +19,7 @@ async function getProgressCards() {
       totalDays: 30,
     },
     {
+      id: '2',
       dueDate: '2024-09-30',
       progress: 0.1,
       title: '딥다이빙 기술 마스터하기',
@@ -33,7 +36,7 @@ export default async function Page() {
     <main className="p-4">
       <h2 className="mb-4 text-xl font-bold">진행중인 목표</h2>
       {cards.map((card) => (
-        <ProgressCard key={card.title} {...card} />
+        <ProgressCard key={card.id} {...card} />
       ))}
     </main>
   )
